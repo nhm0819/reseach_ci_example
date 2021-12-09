5615daa8f387355a431c534d11e19aa8832aaa97
Hongmin <nhm9001@gmail.com>
2021-12-09 21:41:03 +0900

    fix test

--- main_test.py
+++ main_test.py
@@ -4,7 +4,7 @@ import main
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        ret = main.helloworld("Test")
+        ret = main.helloworld("Hong")
         self.assertEqual(ret, "Hello World! Hong!")
 
 if __name__ == "__main__":
